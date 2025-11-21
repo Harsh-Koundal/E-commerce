553fdac14dd307095264c0d473f2427a5efce2c9
Harsh-Koundal <aharsh33039@gmail.com>
2025-11-21 12:22:41 -0800

    bugfix:order

--- printzet-backend/models/AccessoryCategory.js
+++ printzet-backend/models/AccessoryCategory.js
@@ -1,27 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const accessoryCategorySchema = new mongoose.Schema({
-    id: { type: String, required: true },
-    name: { type: String, required: true },
-    description: { type: String },
-    image: { type: String },
-    subcategories: [{
-        id: { type: String, required: true },
-        name: { type: String, required: true },
-        description: { type: String },
-        image: { type: String },
-        material: { type: String },
-        sizeOptions: [{ type: String }],
-        colorOptions: [{ type: String }],
-        customizationOptions: { type: String },
-        baseCost: { type: Number },
-        customizationCosts: { type: Object },
-        availableSizes: [{type: String}],
-        availableColors: [{type: String}]
-    }],
-    dynamicPricing: { type: Boolean },
-    cost: { type: Number },
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  description: String,
+  image: String,
+
+  subcategories: [
+    {
+      id: { type: String, required: true },
+      name: { type: String, required: true },
+      description: String,
+      image: String,
+      material: String,
+      sizeOptions: [String],
+      colorOptions: [String],
+      customizationOptions: String,
+      baseCost: Number,
+      customizationCosts: { type: mongoose.Schema.Types.Mixed },
+      availableSizes: [String],
+      availableColors: [String],
+    },
+  ],
+
+  dynamicPricing: Boolean,
+  cost: Number,
 });
 
-const AccessoryCategory = mongoose.model('AccessoryCategory', accessoryCategorySchema);
+const AccessoryCategory = mongoose.model("AccessoryCategory", accessoryCategorySchema);
 export default AccessoryCategory;
