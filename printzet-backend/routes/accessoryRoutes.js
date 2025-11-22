9cbe91264de59a5ae3bdb7c0b04142cefb703d30
Harsh-Koundal <aharsh33039@gmail.com>
2025-11-22 05:29:17 -0800

    feat:clothing page

--- printzet-backend/routes/accessoryRoutes.js
+++ printzet-backend/routes/accessoryRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAccessoryCategory } from "../controllers/accessoryController.js";
+import { getAccessoryCategory, getClothingCategory } from "../controllers/accessoryController.js";
 
 const router = express.Router();
 
 // Fetch accessory category and its subcategories
 router.get("/accessory-printing", getAccessoryCategory);
+router.get("/clothing-printing", getClothingCategory)
 
 export default router;
   
